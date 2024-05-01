0d516da69b4854dee325b4663516f732a3fd583c
Anirban <abanerjee763@gmail.com>
2024-05-01 22:10:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const morgan = require('morgan');
+const bp = require('body-parser');
+const cp = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const fs = require("fs")
+const cron = require('node-cron');
+require('dotenv').config(); 
+
+
+// app
+const app = express();
+
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true}).then(() => console.log('DB connected'));
+
+// middlewares
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(cp())
+
+
+//cors
+app.use(cors());    
+
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError'){
+        res.status(401).json({
+            error: "Unauthorized!"
+        });
+    }
+});
+
+
+app.get('/', (req,res) => {
+    fs.readFile('apiDocs/docs.json', (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            })
+        }
+
+        const docs = JSON.parse(data);
+        res.json(docs);
+    })
+})
+
+
+// handle port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
